19ac925ce646c4ac13aa38ce87be9d8ad4f28fa7
Vladimir Koledin <vkoledin04@yahoo.com>
2024-06-16 19:23:07 -0400

    adddd

--- static/data.js
+++ static/data.js
@@ -2,17 +2,32 @@ let countries = [
     { name: : "United States of America",
       location : [37.09024, -95.712891]
     },
-    { name: "",
-      location : []
+    { name: "Spain",
+      location : [40.4637,3.7492]
     },
-    { name: "",
-      location :[]
+    { name: "Canada",
+      location :[56.1304,106.3468]
     },
-    { name: "",
-      location : []
+    { name: "United Kingdom ",
+      location : [55.3781,3.4360]
     },
-    { name: "",
-      location : []
+    { name: "Australia",
+      location : [25.2744,133.7751]
+    },
+    { name: "Germany",
+      location : [51.1657,10.4515]
+    },
+    { name: "France",
+      location : [46.2276,2.2137]
+    },
+    { name: "Brzil",
+      location : [14.2350,51.9253]
+    },
+    { name: "Mexico",
+      location : [23.6345,102.5528]
+    },
+    { name: "Italy",
+      location : [41.8719,12.5674]
     }
 
 

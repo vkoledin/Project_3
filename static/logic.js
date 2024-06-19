5490ef2e62feabe9d6c5ae82eafb47173519e914
Gail <gaildichie@Gails-MBP.attlocal.net>
2024-06-19 17:27:52 -0500

    added new file untitled

--- static/logic.js
+++ static/logic.js
@@ -1,36 +1,30 @@
-let myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
-  });
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-
-let countryMarkers = [];
-
-for (let i = 0; i < countries.length; i++) {
-  
-  countryMarkers.push(
-    L.marker(country[i].location).bindPopup("<h1>" + country[i].name + "</h1>")
-  );
-}
-
-  
-  
-  
-  L.circle(countries[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: '#F0F8FF'
-    weight :2
-   
-  }).addTo(myMap);
 
 
+//Use d3 to fetch the Json data
+d3.json("countries.json").then(countries => {
+    //Initialize the map 
+    let myMap = L.map("map", {
+        center: [20, 0],
+        zoom: 2
+    });
 
-  THIS ONE IS NOT DONE 
-  THIS ONE IS NOT DONE 
-  THIS ONE IS NOT DONE 
-  THIS ONE IS NOT DONE 
-  THIS ONE IS NOT DONE 
+    //Adding the tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+
+    //The heatMap data
+      let heatData = countries.map(country => [
+          country.location[0],
+          country.location[1],
+          country.Netflix_users
+      ]);
+
+    //Add the heatmap layer
+    L.heatLayer(heatData, {
+        radius: 25,
+        blur: 15,
+        maxZoom: 10,
+      }).addTo(myMap);
+
+});
